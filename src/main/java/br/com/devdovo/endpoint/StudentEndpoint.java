4e1ee7f13c94d9cfa8cb650c8c1992f32dce6959
Cicero Oliverio <cicero_oliverio@hotmail.com>
2019-04-13 14:51:30 -0300

    Create PUT,POST and DELETE.

--- src/main/java/br/com/devdovo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdovo/endpoint/StudentEndpoint.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,31 @@ public class    StudentEndpoint {
         if(index == -1)
             return new ResponseEntity<>(new CustomErrorType("Student not Found"),HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(Student.studentList.get(index),HttpStatus.OK);
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student){
+
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student,HttpStatus.OK);
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@RequestBody Student student){
+
+        Student.studentList.remove(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<?> update(@RequestBody Student student){
+
+        Student.studentList.remove(student);
+        Student.studentList.add(student);
+        return new ResponseEntity<>(HttpStatus.OK);
 
     }
 
